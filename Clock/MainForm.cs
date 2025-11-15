4da8920bee5f183e0c3c5c1fa05a27423443b6e6
Ada <Ada@PROBOOK>
2025-11-15 15:28:58 +0700

    Add project files.

---
+++ Clock/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clock
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void timer_Tick(object sender, EventArgs e)
+		{
+			//labelTime.Text = DateTime.Now.ToString("HH:mm:ss"); //24-час
+			labelTime.Text = DateTime.Now.ToString("hh:mm:ss tt", 
+				System.Globalization.CultureInfo.InvariantCulture); //12-час AM-PM
+			if (checkBoxShowDate.Checked)
+			{
+				labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+			}
+			if (checkBoxShowWeekday.Checked)
+			{ 
+				labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
+			}
+		}
+
+		private void buttonHideControls_Click(object sender, EventArgs e)
+		{
+			checkBoxShowDate.Visible = false;
+			checkBoxShowWeekday.Visible = false;
+
+		}
+	}
+}
